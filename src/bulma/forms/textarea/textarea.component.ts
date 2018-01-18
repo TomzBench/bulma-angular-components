9830cbdbd3e99b21e2d27fd072afc8ceec3058f3
thoams <thomas.chiantia@gmail.com>
2018-01-18 09:50:22 -0500

    stubs

---
+++ src/bulma/forms/textarea/textarea.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'b-textarea',
+  template: `
+    <p>
+      textarea works!
+    </p>
+  `,
+  styles: []
+})
+export class TextareaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/bulma/tabs/tabs.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'b-tabs',
+  template: `
+    <p>
+      tabs works!
+    </p>
+  `,
+  styles: []
+})
+export class TabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
