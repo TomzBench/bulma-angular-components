8f337792f0b96a1758c6f2ba68bc3bad4d24bc12
thoams <thomas.chiantia@gmail.com>
2018-01-21 22:49:04 -0500

    removed comments

--- src/bulma/tabs/tabs-item.component.ts
+++ src/bulma/tabs/tabs-item.component.ts
@@ -9,30 +9,6 @@ import {
 import { BulmaTabsActiveContext } from './tabs-active.class';
 
 /**
- * A list component dynamically created (For internal use)
- */
-//@Component({
-//  template: `
-//  <li [ngClass]="{'is-active':data.active.label===data.label}">
-//    <a (click)="data.whenClicked()">
-//      <span *ngIf="data.icon" class="icon">
-//        <i class="fa fa-{{data.icon}}"></i>
-//      </span>
-//      <span>{{data.label}}</span>
-//    </a>
-//  </li>
-//  `
-//})
-//export class BulmaTabsItemListComponent implements OnInit {
-//
-//  data: BulmaTabsItemViewComponent;
-//
-//  constructor() {}
-//  ngOnInit() {}
-//
-//}
-
-/**
  * ###Usage
  * <div b-tabs>
  *   <div b-tabs-item="'Tab 0'">
@@ -54,9 +30,7 @@ export class BulmaTabsItemViewComponent implements OnInit {
 
   @Input('b-tabs-item') label: string;
   @Input() icon: string;
-  //@Output() tabClick: EventEmitter < string > ;
   active: BulmaTabsActiveContext;
-  //whenClicked(): void { this.tabClick.emit(this.label); }
   constructor() {}
   ngOnInit() {}
 
